18ea44e7f4495bc8d6c8a3849d7001b161692737
IzanamiiDevv <rafaeloli596@gmail.com>
2024-10-10 22:55:22 +0800

    Add BinaryAppend

--- binarylib.h
+++ binarylib.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define binaryWrite(_Filename, _Type, _Buffer, _ElementSize) { \
-    FILE* fstream = fopen((_Filename), "ab"); \
+    FILE* fstream = fopen((_Filename), "wb"); \
     if(fstream == NULL) { \
         printf("Error: Can't connect to the file: %s\n", _Filename); \
         exit(EXIT_FAILURE); \
@@ -14,6 +14,27 @@
 }
 
 void binaryWriteRef(const char* _Filename, const void* _Buffer, size_t _ElementSize) {
+    FILE* fstream = fopen(_Filename, "wb");
+    if(fstream == NULL) {
+        printf("Error: Can't connect to the file: %s\n", _Filename);
+        exit(EXIT_FAILURE);
+    }
+    fwrite(_Buffer, _ElementSize, 1, fstream);
+    fclose(fstream);
+}
+
+#define binaryAppend(_Filename, _Type, _Buffer, _ElementSize) { \
+    FILE* fstream = fopen((_Filename), "ab"); \
+    if(fstream == NULL) { \
+        printf("Error: Can't connect to the file: %s\n", _Filename); \
+        exit(EXIT_FAILURE); \
+    } \
+    _Type databuff = (_Type)(_Buffer); \
+    fwrite(&(databuff), (_ElementSize), 1, (fstream)); \
+    fclose(fstream); \
+}
+
+void binaryAppendRef(const char* _Filename, const void* _Buffer, size_t _ElementSize) {
     FILE* fstream = fopen(_Filename, "ab");
     if(fstream == NULL) {
         printf("Error: Can't connect to the file: %s\n", _Filename);
@@ -32,4 +53,30 @@ void binaryRead(const char* _FileName, unsigned long _Address, void* _Location, 
     fseek(fstream, _Address, SEEK_SET);
     fread(_Location, _ElementSize, 1, fstream);
     fclose(fstream);
+}
+
+void* binaryReadByValue(const char* _FileName, unsigned long _Address, size_t _ElementSize) {
+    FILE* fstream = fopen(_FileName, "rb");
+    if(fstream == NULL) {
+        printf("Error: Can't connect to the file: %s\n", _FileName);
+        exit(EXIT_FAILURE);
+    }
+
+    void* buff = malloc(_ElementSize);
+    if (buff == NULL) {
+        printf("Error: Memory allocation failed.\n");
+        fclose(fstream);
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(fstream, _Address, SEEK_SET);
+    size_t readCount = fread(buff, _ElementSize, 1, fstream);
+    if (readCount != 1) {
+        printf("Error: Failed to read data from file.\n");
+        free(buff);
+        fclose(fstream);
+        exit(EXIT_FAILURE);
+    }
+    fclose(fstream);
+    return buff;
 }
--- src/main.test.c
+++ src/main.test.c
@@ -5,9 +5,13 @@ int main() {
     binaryWrite("test.exe", int, 10, sizeof(int));
 
     int x;
-    binaryRead("test.exe", 0, &x, sizeof(int));
+    binaryRead("test.exe", 0x0, &x, sizeof(int));
     
     printf("%i\n", x);
+
+    int* y = (int*)binaryReadByValue("test.exe", 0x0, sizeof(int));
+    printf("%i\n", *y);
+    free(y);
 
     return 0;
 }
