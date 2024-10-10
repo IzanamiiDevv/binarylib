0e464af2bfdc5dce55779924aba485dffb47fc19
IzanamiiDevv <rafaeloli596@gmail.com>
2024-10-10 22:05:51 +0800

    Minor Adjustment

--- binarylib.h
+++ binarylib.h
@@ -3,27 +3,32 @@
 #include <stdlib.h>
 
 #define binaryWrite(_Filename, _Type, _Buffer, _ElementSize) { \
-    if(sizeof(_Filename) != sizeof(const char*) \
-    || sizeof(_ElementSize) != sizeof(size_t)) return; \
-    FILE* fstream = fopen((_Filename), "ab");\
-    if(fstream == NULL) {printf("Error: Cant connect to the File."); return;}\
+    FILE* fstream = fopen((_Filename), "ab"); \
+    if(fstream == NULL) { \
+        printf("Error: Can't connect to the file: %s\n", _Filename); \
+        exit(EXIT_FAILURE); \
+    } \
     _Type databuff = (_Type)(_Buffer); \
     fwrite(&(databuff), (_ElementSize), 1, (fstream)); \
     fclose(fstream); \
 }
 
-void binaryWriteRef(const char* _Filename, const void* _Buffer, size_t _ElementSize){
+void binaryWriteRef(const char* _Filename, const void* _Buffer, size_t _ElementSize) {
     FILE* fstream = fopen(_Filename, "ab");
-    if(fstream == NULL) {printf("Error: Cant connect to the File.");return;}
-
+    if(fstream == NULL) {
+        printf("Error: Can't connect to the file: %s\n", _Filename);
+        exit(EXIT_FAILURE);
+    }
     fwrite(_Buffer, _ElementSize, 1, fstream);
     fclose(fstream);
 }
 
-void binaryRead(const char* _FileName, unsigned long _Address , void* _Location, size_t _ElementSize){
+void binaryRead(const char* _FileName, unsigned long _Address, void* _Location, size_t _ElementSize) {
     FILE* fstream = fopen(_FileName, "rb");
-    if(fstream == NULL) {printf("Error: Cant connect to the File.");return;}
-
+    if(fstream == NULL) {
+        printf("Error: Can't connect to the file: %s\n", _FileName);
+        exit(EXIT_FAILURE);
+    }
     fseek(fstream, _Address, SEEK_SET);
     fread(_Location, _ElementSize, 1, fstream);
     fclose(fstream);
--- src/main.test.c
+++ src/main.test.c
@@ -3,6 +3,11 @@
 
 int main() {
     binaryWrite("test.exe", int, 10, sizeof(int));
+
+    int x;
+    binaryRead("test.exe", 0, &x, sizeof(int));
     
+    printf("%i\n", x);
+
     return 0;
 }
